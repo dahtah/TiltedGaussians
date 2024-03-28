49766b03be75b3acbf70fdcaa5ce34a05184a3e3
Simon Barthelmé <simon.barthelme@gipsa-lab.fr>
2024-03-28 09:28:53 +0100

    clean-up

--- src/TiltedGaussians.jl
+++ src/TiltedGaussians.jl
@@ -2,4 +2,5 @@ module TiltedGaussians
 using LinearAlgebra,Statistics,Distributions,PDMats
 include("quadrule.jl")
 include("moments.jl")
+export QuadRule,nnodes,tilted_moments
 end # module TiltedGaussians
